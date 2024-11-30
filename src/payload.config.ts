1e48a5fb446c27a5a8e122f07a3ec29da8e12226
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2024-11-29 20:14:12 -0800

    trying to fix API key

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -9,6 +9,7 @@ import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { s3Storage } from '@payloadcms/storage-s3'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -31,7 +32,23 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
-  ],
-})
+          s3Storage({
+            collections: {
+              prefix: 'media',
+            },
+          },
+            bucket: process.env.S3_BUCKET,
+            config: {
+              credentials: {
+                //@ts-ignore
+                accessKeyId: process.env.S3_ACCESS_KEY_ID,
+                //@ts-ignore
+                secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+              },
+              region: process.env.S3_REGION || 'us-west-1',
+              endpoint: process.env.S3_ENDPOINT,
+              forcePathStyle: true,
+            },
+          }),
+        ],
+
